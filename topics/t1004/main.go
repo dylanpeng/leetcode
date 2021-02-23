b585422febd567316429c766a04579b24b591314
Dylan <peng_dylan@sina.com>
2021-02-23 18:10:56 +0800

    20210223 upgrade

--- topics/t1004/main.go
+++ topics/t1004/main.go
@@ -7,13 +7,13 @@ import (
 )
 
 func main() {
+	r := 0
 	start := time.Now()
 
 	for i := 0; i < 10; i++ {
-		r := result.LongestOnes([]int{0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1}, 3)
-		fmt.Printf("%d \n", r)
+		r = result.LongestOnes([]int{0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1}, 3)
 	}
 
 	duration := time.Since(start).Nanoseconds()
-	fmt.Printf("duration: %d ns\n", duration)
+	fmt.Printf("duration: %d ns\n"+"result:%d\n", duration, r)
 }
