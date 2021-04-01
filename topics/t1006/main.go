f20c45d205c408976b18e5ce92a0a7181c9bd00d
Dylan <peng_dylan@sina.com>
2021-04-01 15:13:17 +0800

    20210401 leetcode t1006

---
+++ topics/t1006/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"leetcode/topics/t1006/result"
+	"time"
+)
+
+func main() {
+	r := 0
+	start := time.Now()
+
+	for i := 0; i < 1; i++ {
+		r = result.Clumsy(4)
+	}
+
+	duration := time.Since(start).Nanoseconds()
+	fmt.Printf("duration: %d ns\n"+"result:%+v\n", duration, r)
+}
